931f1620299d0178b94de464e03d637035eabfd8
chapeter <chapeter@cisco.com>
2015-12-17 10:44:32 -0600

    added logic to get interface network and subnet.  VIP address now uses proper mask...currenly only works for 1 vip and 1 ip per interface

--- acimigrate/tasks.py
+++ acimigrate/tasks.py
@@ -1,10 +1,14 @@
 import logging
+import ipaddress
 
 logger = logging.getLogger(__name__)
 
 logger.info('Loading Tasks')
 def migrate(nx, apic, auto=True, layer3=False):
     migration_dict = nx.migration_dict()
+    #print "********"
+    #print migration_dict
+    #print "********"
     result = {}
     for v in migration_dict.keys():
         name = migration_dict[v]['name']
@@ -20,10 +24,20 @@ def migrate(nx, apic, auto=True, layer3=False):
                 print 'Failed to create EPG for vlan {}'.format(name)
                 result[name] = 'FAILED'
             if layer3 and hsrp:
+                    count = 0
                     for vip in hsrp['vips']:
+                        ip = unicode(vip)
+                        subnet = unicode(hsrp['subnets'][count]+"/"+str(hsrp['masks'][count]))
+                        if ipaddress.ip_address(ip) in ipaddress.ip_network(subnet):
+                            mask = str(hsrp['masks'][count])
+                        else:
+                            mask = "24"
+                            #TODO: Add some better failure logic
+                            #TODO: once multiple IP and VIP logic added, need to find proper subnet mask in list
                         tenant = apic.create_epg_for_vlan(name,
                                                           mac_address=hsrp['vmac'],
-                                                          net=vip+'/24') #TODO: need to get mask dynamically
+                                                          net=vip+'/'+mask)
+                        count = count + 1
                         if tenant.ok:
                             logger.info('Layer 3 migration for {} vlan completed'.format(name))
                             print 'Layer 3 migration for {} vlan completed'.format(name)
